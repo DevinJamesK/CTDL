1d2aee15ffdac96c240dc1a3e634443180bfa2ee
dkloos <dkloos@globemotors.com>
2017-03-31 12:40:10 -0400

    Pulls current sample json data, working

--- CTDL.py
+++ CTDL.py
@@ -1,3 +1,25 @@
-!#/usr/bin/python
+#!/usr/bin/python3
 
-import OS
+import sys
+import os
+import requests
+import json
+
+def main():
+    init()
+
+def init():
+    if not os.path.exists('./samples'):
+        print ("Creating sample directory: ", end='')
+        os.makedirs('./samples')
+        print ("DONE")
+
+    print ("Updating sample data: ", end='')
+    r = requests.get('https://motian.indabamusic.com/samples')
+    if (not r.status_code is 200):
+        print("FAILED, Server Returned {0}".format(r.status_code))
+        exit()
+    print("DONE")
+
+if __name__ == '__main__':
+    main()
