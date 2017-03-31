c3570c0ab3ccb49287774e2c7a14209b0d9f91e2
dkloos <dkloos@globemotors.com>
2017-03-31 12:55:45 -0400

    Init function complete, current sample data loaded on each run

--- CTDL.py
+++ CTDL.py
@@ -6,20 +6,23 @@ import requests
 import json
 
 def main():
-    init()
+    sampleData = init()
 
 def init():
+    # Creating our sample folder where our downloads will be stored
     if not os.path.exists('./samples'):
-        print ("Creating sample directory: ", end='')
+        print ("Creating sample directory if one doesn't exist: ", end='')
         os.makedirs('./samples')
         print ("DONE")
 
-    print ("Updating sample data: ", end='')
+    # Pulling most current json sample data from below URL
+    print ("Updating sample data to most current: ", end='')
     r = requests.get('https://motian.indabamusic.com/samples')
     if (not r.status_code is 200):
         print("FAILED, Server Returned {0}".format(r.status_code))
         exit()
     print("DONE")
+    return r.json();
 
 if __name__ == '__main__':
     main()
